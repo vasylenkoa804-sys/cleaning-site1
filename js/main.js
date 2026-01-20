54b8ca069329abcd70ae339e1a858de36155ef50
vasylenkoa804-sys <vasylenkoa804@gmail.com>
2026-01-20 22:56:37 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log("Cleaning site loaded successfully");
+
+const map = L.map('map', {
+    center: [49.8397, 24.0297], // Львів
+    zoom: 8
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+}).addTo(map);
